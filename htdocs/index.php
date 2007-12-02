ee1ce703db81a4e5b9c3c50ab9354c69ba78dbe2
kbx <kbx@users.sourceforge.net>
2007-12-02 11:59:20 +0000

    Comment beta versions as there is no beta available at the moment.

--- htdocs/index.php
+++ htdocs/index.php
@@ -19,7 +19,9 @@
       <img src="images/RunJabRef.png" alt="Run JabRef instantly" /></a>
 
       <a href="http://sourceforge.net/project/showfiles.php?group_id=92314&amp;package_id=97632&amp;release_id=552736"><img src="images/Download-stable.png" alt="Download current stable version" /></a>
-      <a href="download.php#beta"><img src="images/Download-beta.png" alt="Download latest beta version" /></a>
+      <?php 
+      // <a href="download.php#beta"><img src="images/Download-beta.png" alt="Download latest beta version" /></a>
+      ?>
 
       <h3 id="News">News</h3>
 
