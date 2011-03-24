69038a2bb4728273fd6c9f2b14198193ae873dbe
kbx <kbx@users.sourceforge.net>
2011-03-24 07:06:49 +0000

    Activate beta download for version 2.7 beta.

--- htdocs/download.php
+++ htdocs/download.php
@@ -24,16 +24,14 @@ are beta-tested and expected to be stable and reliable.</p>
       <a href="http://sourceforge.net/projects/jabref/files/jabref/2.6"><img src="images/Download-stable.png" alt="Download current stable version" /></a>
     </p>
 
-<?php /*
     <h3 id="beta">Current beta version</h3>
     <p>The beta version is an alternative for users who can't wait for the latest features, or who want to help out with beta-testing.</p>
     <p class="red">
      If you use the beta version, please be prepared that it can be unstable and contain serious bugs. Make backups of all important data!
     </p>
     <p>
-      <a href="http://sourceforge.net/projects/jabref/files/jabref/2.6%20beta%203/"><img src="images/Download-beta.png" alt="Download latest beta version" /></a>
+      <a href="http://sourceforge.net/projects/jabref/files/jabref/2.7%20beta/"><img src="images/Download-beta.png" alt="Download latest beta version" /></a>
     </p>
-*/ ?>
 
     <h3 id="subversion">Developer versions</h3>
     <p>
