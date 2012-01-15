21e6d4372e59bca7f0b05042e6618f9447ad469e
olly98 <olly98@users.sourceforge.net>
2012-01-15 19:48:16 +0100

    help conversion script: enhanced and added recursion into sub directories  * external http references are not rewritten any more  * changed charset declaration to UTF8 (all files are coded in UTF8)  * changed content type to applicatoin/xhtml+xml  * stripping html tags within headings  * removed basefont handling: basefont is always one line  * changed XHTML declaration to XHTML version 1.1  * added enforcement of Windows newlines (required at cygwin environments)

--- generate-htdocs-help-from-jabref-src-help.pl
+++ generate-htdocs-help-from-jabref-src-help.pl
@@ -1,35 +1,119 @@
 #!/usr/bin/perl
 
-if (@ARGV==0) {
+use constant HELPDIR_JABREF => "jabref/src/help";
+use constant HELPDIR_WEB    => "htdocs/help";
 
-  print "Please provide name of .html file as commandline argument!\n";
+#required at cygwin
+use constant FORCE_WINDOWS_NEWLINES => 1;
 
-} else {
+use warnings;
+use strict;
 
-  my $infilename  =  $ARGV[0];
-  my $outfilename =  $infilename;
-  $outfilename =~ s/\.html/\.php/g;
+sub handleDir;
+sub handleFile;
 
-  open(INFILE,$infilename);
-  @infile=<INFILE>;
-  close(INFILE);
+#Debug call for a single file
+#handleFile("jabref/src/help/About.html", "htdocs/help/About.php", "en");
+#exit;
 
+
+# handle English
+handleDir(HELPDIR_JABREF, HELPDIR_WEB, "en");
+
+#handle other languages (contained in sub directories)
+
+my $helpdirJabRef;
+
+opendir($helpdirJabRef, HELPDIR_JABREF) or die $!;
+
+my $sourcedir;
+my $targetdir;
+my $lang;
+
+while (my $subdir = readdir($helpdirJabRef)) {
+	$sourcedir = HELPDIR_JABREF . "/$subdir";
+	next unless (-d $sourcedir);
+	next if ($subdir =~ /\.\.?/);
+	
+	$targetdir = HELPDIR_WEB . "/$subdir";
+	$lang = $subdir;
+	
+	handleDir($sourcedir, $targetdir, $lang);
+}
+close($helpdirJabRef);
+
+
+# Parameters:
+#    sourcedir
+#    targetdir
+#    language
+sub handleDir {
+	my $sourcedir = shift;
+	my $targetdir = shift;
+	my $lang = shift;
+	
+	print("Handling $sourcedir...\n");
+	
+	if (!-d $targetdir) {
+		mkdir($targetdir);
+	}
+
+	my $dh;
+	opendir($dh, $sourcedir) or die $!;
+	while (my $infilename = readdir($dh)) {
+		next unless ($infilename =~ /\.html$/);
+		my $outfilename =  $infilename;
+		$outfilename =~ s/\.html/\.php/g;
+		my $sourcefilename = $sourcedir . "/" . $infilename;
+		my $targetfilename = $targetdir . "/" . $outfilename;
+		handleFile($sourcefilename, $targetfilename, $lang);
+	}
+	close($dh);	
+}
+
+#
+# Parameters:
+#    infilename: source file (html)
+#    outfile:    target file (php)
+#    lang:       language (ISO-format)
+#
+sub handleFile {
+  my $infilename  =  shift;
+  my $outfilename = shift;
+  my $lang = shift;
+  
+  #Debug output
+  #print("handleFile:\n$infilename\n$outfilename\n$lang\n\n");
+
+  open(my $infileH, "<", $infilename) or die "cannot open < $infilename: $!";
+  my @infile = <$infileH>;
+  
   my @outfile=();
 
-  my $titel="";
+  # Determine title out of first h1 heading
+  my $title="";
+  my $line;
   foreach $line(@infile) {
     if ($line =~ /\<h1\>(.*)\<\/h1\>/) {
-      $titel=$1;
+      $title=$1;
+	  # title is found, go to the normal handling
+	  last;
     }
   }
-
+  
+  #remove html tags from title
+  #even if <em> is not allowed in h1 elements, JabRef doc uses that
+  $title =~ s#<(.|\n)*?>##g;
+  
   my $header=<<HTML;
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">
+<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.1//EN\"
+    \"http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xml:lang=\"$lang\">
 <head>
-  <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />
-  <title>$titel</title>
-  <link href='/css/style.css' rel='stylesheet' type='text/css' />
+  <meta http-equiv=\"content-type\" content=\"application/xhtml+xml; charset=UTF-8\" />
+  <title>$title</title>
+  <link href=\"/css/style.css\" rel=\"stylesheet\" type=\"text/css\" />
 </head>
 
 <body>
@@ -47,31 +131,48 @@ HTML
   push(@outfile, $header);
 
   my $status=0;
+  # 0 out of html
+  # 1 in html
+  # 2 within basefont
+  
   foreach $line(@infile) {
+    #Debug states
+	#print "$status / $line";
 
-    if ($status==1 && $line =~ /\<\/body\>/) {
-      $status=0;
-    }
-
-    if ($status==1) {
-      $line =~  s/href=\"(.*)\.html\"/href=\"$1\.php\"/g;
-      push(@outfile, $line);
-    }
-
-    if ($status==0 && $line =~ /\<basefont/) {
-      $status=2;
-    }
-
-    if ($status==2 && $line =~ /\/\>/) {
+    if ($status==0 && $line =~ /\<body/) {
       $status=1;
+    } elsif ($status==1 && $line =~ /\<\/body\>/) {
+      $status=0;
+    } elsif ($status==1) {
+	  if (!($line =~ /href=\"http:\/\//)) {
+		#line does NOT contain a href to some http address
+		#we assume that line is NOT a reference to an external site
+		#replace "html" extension with "php" extension
+		#still allow links as "...html#part".
+		$line =~  s/href=\"([^\"]*)\.html/href=\"$1\.php/g;
+	  }
+      push(@outfile, $line);
+    #} elsif ($status==0 && $line =~ /\<basefont/) {
+    #  $status=2;
+    #} elsif ($status==2 && $line =~ /\/\>/) {
+    #  $status=1;
     }
-
   }
 
   push(@outfile, $footer);
 
   open(OUTFILE,">$outfilename");
-  print OUTFILE @outfile;
-  close(OUTFILE);
+  
+  if (FORCE_WINDOWS_NEWLINES) {
+	foreach my $line (@outfile) {
+		$line =~ s/\r?\n|\r/\r\n/g;
+	}
+  }
 
+  print OUTFILE @outfile;
+
+  close(OUTFILE);
+  
+  close($infileH);
 }
+
