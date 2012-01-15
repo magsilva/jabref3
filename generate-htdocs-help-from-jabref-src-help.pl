e3b94f9a49f9d5762f468fd59f150fbea9bf9d97
olly98 <olly98@users.sourceforge.net>
2012-01-15 11:19:00 +0100

    help generation script: html header split in multiple lines

--- generate-htdocs-help-from-jabref-src-help.pl
+++ generate-htdocs-help-from-jabref-src-help.pl
@@ -23,8 +23,26 @@ if (@ARGV==0) {
     }
   }
 
-  my $header="<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">\n\n<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">\n<head>\n  <meta http-equiv=\"content-type\" content=\"text/html; charset=ISO-8859-1\" />\n  <title>$titel</title>\n  <link href='/css/style.css' rel='stylesheet' type='text/css' />\n</head>\n\n<body>\n  <div id=\"container\">\n    <?php include(\"../navigation.php\"); ?>\n    <a href=\"Contents.php\">Back to contents</a>\n\n";
-  my $footer="    <?php include(\"../footer.php\"); ?>\n  </div>\n\n</body>\n</html>\n";
+  my $header=<<HTML;
+<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+<html xmlns=\"http://www.w3.org/1999/xhtml\" lang=\"en\" xml:lang=\"en\">
+<head>
+  <meta http-equiv=\"content-type\" content=\"text/html; charset=UTF-8\" />
+  <title>$titel</title>
+  <link href='/css/style.css' rel='stylesheet' type='text/css' />
+</head>
+
+<body>
+  <div id=\"container\">
+    <?php include(\"../navigation.php\"); ?>
+    <a href=\"Contents.php\">Back to contents</a>
+	
+HTML
+
+  my $footer=<<HTML;
+  <?php include(\"../footer.php\"); ?>
+  </div>\n\n</body>\n</html>
+HTML
 
   push(@outfile, $header);
 
