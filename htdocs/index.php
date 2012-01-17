c253f2290230c33ca4c2b827f1cc267207f2b1b6
John Doe <kbx@users.sourceforge.net>
2012-01-17 10:28:30 +0100

    Announce version 2.8 beta and add revision history.

--- htdocs/index.php
+++ htdocs/index.php
@@ -29,11 +29,14 @@
 
       <a href="http://sourceforge.net/projects/jabref/files/jabref/2.7.2"><img src="images/Download-stable.png" alt="Download current stable version" /></a>
 
-<?php // <a href="download.php#beta"><img src="images/Download-beta.png" alt="Download latest beta version" /></a> ?>
+      <a href="download.php#beta"><img src="images/Download-beta.png" alt="Download latest beta version" /></a>
 
       <h3 id="News">News</h3>
 
       <ul class="history_column">
+
+	<li class="item">January 05th, 2012</li>
+	<li class="item_desc">Version 2.8 beta</li>
 
 	<li class="item">November 1st, 2011</li>
 	<li class="item_desc">Version 2.7.2</li>
