1aae3015ffcf50d155e0aad3904a7c37913af363
Oliver Kopp <olly98@users.sourceforge.net>
2012-03-17 18:59:56 +0100

    htdocs: regenerated help files

--- htdocs/help/de/About.php
+++ htdocs/help/de/About.php
@@ -6,7 +6,7 @@
 ?>
 <head>
   <meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
-  <title>JabRef Version 2.8b</title>
+  <title>JabRef Version 2.8b2</title>
   <link href="/css/style.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -15,7 +15,7 @@
     <?php include("../../navigation.php"); ?>
     <a href="Contents.php">Zur&uuml;ck zum Inhaltsverzeichnis</a>
 
-        <h1>JabRef Version 2.8b</h1>
+        <h1>JabRef Version 2.8b2</h1>
 
         <div id="logo">
         <img src="/images/JabRef-icon-48.png" alt="JabRef logo"/><br/>&copy; 2003-2012
