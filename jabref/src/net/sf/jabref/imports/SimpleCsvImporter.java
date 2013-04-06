a8a84fb883db7a4166bcc47d1c19ebf0f6ad2785
Marco Aurélio Graciotto Silva <magsilva@gmail.com>
2013-04-06 15:17:25 -0300

    - Reorganize the tree.   - Remove any file that can be automatically generated.   - Remove htdocs (Home-Page).   - Move plugins code to a different directory (/plugins)   - Move JabRef specific code into /jabref directory.   - Remove libraries (OpenOffice/LibreOffice UNO Java libraries and Docear/SCIPLORE libraries).   - Update UI to use newer JGoodies Form (*Button2 -> *Button).   - Remove code from JPFCodeGen (it is an external project).   - Move test cases to a different directory (jabref/test).   - Mavenize the JabRef module:     - Update dependencies to newer package versions.     - Set up automatic generation of BST and Search-related code (ANTLR).

---
+++ jabref/src/net/sf/jabref/imports/SimpleCsvImporter.java
@@ -0,0 +1,39 @@
+package net.sf.jabref.imports;
+
+import java.io.*;
+import java.util.*;
+
+import net.sf.jabref.*;
+import net.sf.jabref.imports.ImportFormat;
+import net.sf.jabref.imports.ImportFormatReader;
+
+public class SimpleCsvImporter extends ImportFormat {
+
+  public String getFormatName() {
+    return "Simple CSV Importer";
+  }
+
+  public boolean isRecognizedFormat(InputStream stream) throws IOException {
+    return true; // this is discouraged except for demonstration purposes
+  }
+  
+  public List<BibtexEntry> importEntries(InputStream stream, OutputPrinter status) throws IOException {    
+    List<BibtexEntry> bibitems = new ArrayList<BibtexEntry>();
+    BufferedReader in = new BufferedReader(ImportFormatReader.getReaderDefaultEncoding(stream));
+      
+    String line = in.readLine();
+    while (line != null) {
+      if (!"".equals(line.trim())) {
+        String[] fields = line.split(";");
+        BibtexEntry be = new BibtexEntry(Util.createNeutralId());
+        be.setType(BibtexEntryType.getType("techreport"));
+        be.setField("year", fields[0]);
+        be.setField("author", fields[1]);
+        be.setField("title", fields[2]);
+        bibitems.add(be);
+        line = in.readLine();
+      }     
+    }
+    return bibitems;      
+  }
+}
